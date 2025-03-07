bc5bc8b85ccb4747069c28532ef1abc700cf18ed
teotoivo <teo.maximilien@gmail.com>
2025-03-08 01:50:57 +0200

    removed files that are in gitignore

---
+++ build/CMakeFiles/ttsh.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "/home/teotoivo/Projects/TeoToivoShell/bin/ttsh"
+  "/home/teotoivo/Projects/TeoToivoShell/bin/ttsh.pdb"
+  "CMakeFiles/ttsh.dir/link.d"
+  "CMakeFiles/ttsh.dir/src/main.c.o"
+  "CMakeFiles/ttsh.dir/src/main.c.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/ttsh.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
